64f6caf6cabb625685b224c5e9a560d412d8b95b
Vitalii <seminenkovs@gmail.com>
2023-10-15 23:15:16 +0200

    Created Model Desk

---
+++ TaskManagerApi/Models/Desk.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerApi.Models
+{
+    public class Desk
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public byte[] Photo { get; set; }
+        public bool IsPrivate { get; set; }
+        public string Columns { get; set; }
+        public User Admin { get; set; }
+    }
+}
