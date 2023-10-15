5292eb9fca8f4995f869ad657178257fd112a918
Vitalii <seminenkovs@gmail.com>
2023-10-15 23:02:40 +0200

    Created Model User

---
+++ TaskManagerApi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerApi.Models;
+
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public DateTime LastLoginDate { get; set; }
+    public byte[] Photo { get; set; }
+}
