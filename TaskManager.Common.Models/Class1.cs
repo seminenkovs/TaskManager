8cedc4595eddf4c5568885bc86b5e15afb8f803b
Vitalii <seminenkovs@gmail.com>
2023-10-22 19:21:44 +0200

    Created library Common.Models

---
+++ TaskManager.Common.Models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManager.Common.Models
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TaskManagerApi/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerApi.Models.Data;
+
+namespace TaskManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationContext _db;
+
+        public UsersController(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateUser([FromBody] object userModel)
+        {
+            if (userModel != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
