14465cfbb23f9c83cc1559df72f888940eca08f6
Vitalii <seminenkovs@gmail.com>
2023-10-29 20:50:44 +0100

    Added method GetToken to AccountController

--- TaskManagerApi/Controllers/AccountController.cs
+++ TaskManagerApi/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TaskManagerApi.Models;
 using TaskManagerApi.Models.Data;
 using TaskManagerApi.Models.Services;
 
@@ -39,7 +42,23 @@ namespace TaskManagerApi.Controllers
             var userData = _userService.GetUserLoginPassFromBasicAuth(Request);
             var login = userData.Item1;
             var pass = userData.Item2;
+            var identity = _userService.GetIdentity(login,pass);
+            var now =DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                issuer:AuthOptions.ISSUER,
+                audience:AuthOptions.AUDIENCE,
+                notBefore: now,
+                claims:identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+            return Ok(response);
         }
     }
 }
