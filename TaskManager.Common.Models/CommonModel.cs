d0b4e6622b015b12a0d0a0aa789c8e0d8f1c68c0
Vitalii <seminenkovs@gmail.com>
2023-11-01 17:54:28 +0100

    CommonModel

--- TaskManager.Common.Models/CommonModel.cs
+++ TaskManager.Common.Models/CommonModel.cs
@@ -2,5 +2,9 @@
 
 public class CommonModel
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreationDate { get; set; }
+    public byte[] Photo { get; set; }
 }
