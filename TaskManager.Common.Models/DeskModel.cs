24a4aaa2ada0350ed6db79e79ca6da77d98f6a1e
Vitalii <seminenkovs@gmail.com>
2023-12-07 18:29:46 +0100

    Desk added method ToDto()

--- TaskManager.Common.Models/DeskModel.cs
+++ TaskManager.Common.Models/DeskModel.cs
@@ -6,5 +6,5 @@ public class DeskModel : CommonModel
     public string[] Columns { get; set; }
     public int ProjectId { get; set; }
     public int AdminId { get; set; }
-    public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
+    public List<int> TasksIds { get; set; }
 }
--- TaskManagerApi/Models/Desk.cs
+++ TaskManagerApi/Models/Desk.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskManagerApi.Models
+﻿using TaskManager.Common.Models;
+
+namespace TaskManagerApi.Models
 {
     public class Desk : CommonObjects
     {
@@ -11,5 +13,22 @@
         public int ProjectId { get; set; }
         public Project Project { get; set; }
         public List<Task> Tasks { get; set; } = new List<Task>();
+
+        public DeskModel ToDto()
+        {
+            return new DeskModel()
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Description = this.Description,
+                CreationDate = this.CreationDate,
+                Photo = this.Photo,
+                AdminId = this.AdminId,
+                IsPrivate = this.IsPrivate,
+                Columns = this.Columns?.Replace("[", "")
+                    .Replace("]", "")
+                    .Split(","),
+            };
+        }
     }
 }
