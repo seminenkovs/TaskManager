977a80339d21e6fb8c15986bb7c8e87289056323
Vitalii <seminenkovs@gmail.com>
2023-12-07 18:20:52 +0100

    Created DesksService

---
+++ TaskManagerApi/Models/Services/DesksService.cs
@@ -0,0 +1,35 @@
+﻿using TaskManager.Common.Models;
+using TaskManagerApi.Models.Data;
+
+namespace TaskManagerApi.Models.Services;
+
+public class DesksService : AbstractionService, ICommonService<DeskModel>
+{
+    private readonly ApplicationContext _db;
+
+    public DesksService(ApplicationContext db)
+    {
+        _db = db;
+    }
+
+    public bool Create(DeskModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(int id, DeskModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DeskModel Get(int id)
+    {
+        Desk desk = _db.Desks.FirstOrDefault(d => d.Id == id);
+        return desk?.ToDto();
+    }
+}
