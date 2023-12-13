d9cd4a470178ab6387e2312a2af3889a5d352afd
Vitalii <seminenkovs@gmail.com>
2023-12-13 23:56:18 +0100

    UsersRequestService  created GetDataByUrl() and GetToken()

--- TaskManager.Client/Services/UsersRequestService.cs
+++ TaskManager.Client/Services/UsersRequestService.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TaskManager.Client.Models;
 
 namespace TaskManager.Client.Services
 {
     class UsersRequestService
     {
-        public void GetToken(string userName, string password)
-        {
+        private const string HOST = "http://localhost:5129/api/";
 
+        private string GetDataByUrl(string url, string userName, string password)
+        {
+            string result = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "POST";
+            if (userName != null && password != null)
+            {
+                string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1")
+                    .GetBytes(userName + ":" + password));
+                request.Headers.Add("Authorization", "Basic " + encoded);
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    string responseStr = reader.ReadToEnd();
+                    return responseStr;
+                }
+            }
+
+            return result;
+        }
+
+        public AuthToken GetToken(string userName, string password)
+        {
+            string url = HOST + "/Account/token";
+            string resultStr = GetDataByUrl(url, userName, password);
+            AuthToken token = JsonConvert.DeserializeObject<AuthToken>(resultStr);
+             
+            return token;
         }
     }
 }
